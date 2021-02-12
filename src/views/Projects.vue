4ffb99622dbee1a70acf9806d9fa95a7b5e65878
Avi <aviblinder@gmail.com>
2021-02-12 19:36:21 +0200

    '$npm_config_comment'

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,10 +1,83 @@
 <template>
   <div>
-    <h1>This is the Projects page</h1>
+    <!-- cards -->
+    <section class="section">
+      <div class="container">
+        <div class="mt-5 columns is-centered is-8 is-variable">
+          <!-- Sentiment Analysis  -->
+          <div class="column is-4-tablet is-3-desktop">
+            <div class="card">
+              <div class="card-image has-text-centered px-3 py-3">
+                <img src="../assets/fishing-net-1526496__180.jpg" 
+                alt="Sentiment Analysis" />
+              </div>
+              <div class="card-content">
+                <p class="title is-size-5 has-text-centered">Sentiment Analysis</p>
+                <span></span>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
+              <router-link 
+                style="margin-left:1rem" 
+                :to="{name: 'sentiment-analysis'}" 
+                tag="a">Show me
+                </router-link>      
+               </p>
+              </footer>
+            </div>
+          </div>
+          <!-- Healthy life dashboard -->
+          <div class="column is-4-tablet is-3-desktop">
+            <div class="card">
+              <div class="card-image has-text-centered px-3 py-3">
+                <img src="../assets/elder-woman-with-hat-400x284.jpg" 
+                alt="Sentiment Analysis" />
+              </div>
+              <div class="card-content">
+                <p class="title is-size-5 has-text-centered">Healthy Life Expectancy</p>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
 
-   <router-link style="margin-left:1rem" :to="{name: 'sentiment-analysis'}" tag="a">Sentiment Analysis</router-link>      
-   <router-link style="margin-left:1rem" :to="{name: 'comparing-healthy-life'}" tag="a">Healthy Life Dashboard</router-link>      
-   <router-link style="margin-left:1rem" :to="{name: 'social-network-analysis'}" tag="a">Social Network Analysis</router-link>      
+     <router-link 
+     style="margin-left:1rem" 
+     :to="{name: 'comparing-healthy-life'}" 
+     tag="a">
+     Show me
+     </router-link>      
+               </p>
+              </footer>
+            </div>
+          </div>
+          <!--  -->
+          <div class="column is-4-tablet is-3-desktop">
+            <div class="card">
+              <div class="card-image has-text-centered px-3 py-3">
+                <img src="../assets/easter-1238465__340-400x284.jpg" 
+                alt="Sentiment Analysis" />
+              </div>
+              <div class="card-content">
+                <p class="title is-size-5 has-text-centered">Social Network Analysis</p>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
+            <router-link 
+              style="margin-left:1rem" 
+              :to="{name: 'social-network-analysis'}" 
+              tag="a">
+              Show me
+              </router-link>    
+               </p>
+              </footer>
+            </div>
+          </div>
+          <!--  -->
+        </div>
+      </div>
+    </section>
+    
+
+
     <router-view> </router-view>
   </div>
 </template>
--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is the Tools page</h1>
+   
+     <section class="section">
+      <div class="container">
+        <div class="mt-5 columns is-centered is-8 is-variable">
+          <!-- Sentiment Analysis  -->
+          <div class="column is-4-tablet is-3-desktop">
+            <div class="card">
+              <div class="card-image has-text-centered px-3 py-3">
+                <img src="../assets/time_is_money-1637393__180.jpg" 
+                alt="Sentiment Analysis" />
+              </div>
+              <div class="card-content">
+                <p class="title is-size-5 has-text-centered">Time Series Forecasts Calculator</p>
+                <span></span>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
+           <router-link 
+            style="margin-left:1rem" 
+            :to="{name: 'time-series-forecasts-calculator'}" 
+              tag="a">
+              Show me
+              </router-link>      
+               </p>
+              </footer>
+            </div>
+          </div>
 
-           <router-link style="margin-left:1rem" :to="{name: 'time-series-forecasts-calculator'}" 
-       tag="a">Time Series Forecasts Calculator</router-link>      
+        </div>
+      </div>
+     </section>
+
 
 
   </div>
