53b3d1b7cf769290023ba19573eb526ddfb0b547
Avi <aviblinder@gmail.com>
2021-02-13 17:01:50 +0200

    '$npm_config_comment'

--- src/Projects/sentimentAnalysis.vue
+++ src/Projects/sentimentAnalysis.vue
@@ -6,25 +6,29 @@
         <div class="column is-3 is-hidden-mobile is-vcentered">
               <projects-side-bar :currentProject="project_id"> </projects-side-bar>
         </div>
-        
-
         <div class="column is-7">
-
-      <article>
-
+          <article>
       <div>
-        <h2>Classification of Yelp User's reviews</h2>
-        <p>&nbsp;</p>
-        <h3><span style="color: #3366ff">Introduction</span></h3>
+                  
+        <h2 class="title has-text-centered tite my-3">Classification of Yelp User's reviews</h2>
+
+        <h3 class="subtitle has-text-centered my-3"><span style="color: #3366ff">Introduction</span></h3>
+      
+        <div class="block">
         <p>
-          The purpose of this project was to compare between a 'basic' sentiment
+          <mark> The purpose of this project is to compare between a 'basic' sentiment
           analysis algorithm and a more elaborate one.
+          </mark>
         </p>
+        </div>
+        <div class="block">
         <p>
           The algorithms were tested thru the 'reviews' done by users (mainly
           for restaurants) as they were supplied by Yelp for 2016's challenge
           ('Round 8') in the 'business' JSON dataset.
         </p>
+        </div>
+        <div class="block">
         <p>
           In this dataset, there are two main fields of interest for this
           project: '<strong>text</strong>' which contains a (qualitative) review
@@ -32,37 +36,63 @@
           is a numeric ranking (ranging from 1 to 5), usually meaning a
           quantitative feedback.
         </p>
+        </div>
+        <div class="block">
+        <div class="subtitle has-text-justified">
+          <span style="color: #3366ff">
+          Measuring <em>'sentiment'</em> 
+          </span>
+        </div>
+</div>
+
+<div class="block">
+  
         <p>
-          <u>'The Sentiment'</u>: The output of both algorithms (the benchmark
+          The output of both algorithms (the benchmark
           and the advanced one) was a classification of each review by one of
           three possible sentiments: 'Positive', 'Neutral' or 'Negative'.
         </p>
+        </div>
+        <div class="block">
+
         <p>
           <u>Validating the results</u>: The validation of each classification
           was done by using the 'stars' that the user gives the business being
           ranked, along with the review.
         </p>
+        </div>
+
+<div class="block">
         <p>
           The underlying interpretation of the connection between qualitative
           and quantitative feedbacks states that a
-          <em>negative review </em>equals one or two stars, a
-          <em>neutral one</em> equals three and a
-          <em>positive review</em> equals four or five stars. Of course, this
+          <strong><em>negative review </em></strong> one or two stars, a
+          <em> <strong>neutral one</strong></em> equals three and a
+          <em><strong>positive review</strong></em> equals four or five stars. 
+          Of course, this
           interpretation is subjective and may differ from user to user, thus
           creating some discrepancies in the validation of the results.
         </p>
-        <h3><span style="color: #3366ff">The Analysis</span></h3>
+</div>
+
+
+        <h3 class="subtitle has-text-centered"><span style="color: #3366ff">The Analysis</span></h3>
         <h4>
-          <span style="color: #3366ff"
+         <span style="font-size: 14pt; line-height: 107%; color: #3366ff"
             >Part 1 &#8211; The 'basic' algorithm.</span
           >
         </h4>
+        <div class="block my-3">
         <p>
-          The basic and relatively fast performing algorithm that I implemented
-          as benchmark works as follow:The basic and relatively fast performing
+          <mark>
+         The basic and relatively fast performing
           algorithm that I implemented as the benchmark works as follow:
+          </mark>
         </p>
+        </div>
+
         <ol>
+<div class="block">
           <li>
             Read a lexicon containing positive and negative words &#8211; the
             list of words is based on
@@ -70,6 +100,8 @@
               >Hu and Liu's lexicon</a
             >
           </li>
+          </div>
+<div class="block">
           <li>
             For each review, create a corpus using the following
             transformations:
@@ -79,24 +111,35 @@
               <li>remove 'english' stop words</li>
             </ul>
           </li>
+</div>
+<div class="block">
           <li>Count the negative and positive words in each corpus.</li>
+</div>
+<div class="block">
           <li>
             Subtract the negative from the positive sums: If a sum &lt; 0 then
             the review is <em>negative.</em> If a sum &gt; 0 then the review is
             <em>positive</em>. Otherwise classify as a neutral review.
           </li>
+</div>
         </ol>
+
+<div class="block">
         <p>
           The classification done by the algorithm is compared against the
           ranking that each user gave, together with the review.
         </p>
+</div>
+<div class="block">
         <p>
           After running a sample of 800,000 reviews, the basic algorithm's
           results are as follows (the colored heatmap should be read
           column-wise: the green thru yellow coloring is per column, going from
           the highest to the lowest values):
         </p>
-        <p>
+</div>
+
+<figure>
           <img
             class="aligncenter wp-image-465 size-full"
             title="Basic Algorithm - Raw Numbers"
@@ -122,46 +165,71 @@
             "
             sizes="(max-width: 492px) 100vw, 492px"
           />
-        </p>
+          <figcaption></figcaption>
+</figure>
+
+<div class="block">
         <p>
           According to the above tables, around<strong> 40.6%</strong> of
           Negative reviews are recognized correctly (1 and 2 stars), and
           <strong>93.4%</strong> of positive reviews are also recognized
           properly (4 and 5 stars).
         </p>
+</div>
+
+<div class="block">
         <p>
           The execution of the analysis was performed on an
-          <strong>m4.4xlarge</strong> EC2 instance, and took about one
-          <strong>hour and 40 minutes</strong>.
+          <strong>m4.4xlarge</strong> EC2 instance, and took about 
+          <strong>one hour and 40 minutes</strong>.
         </p>
+</div>        
+
+<div>
+
         <p>
           <span style="font-size: 14pt; line-height: 107%; color: #3366ff"
             >Part 2 &#8211; The 'advanced' algorithm.</span
           >
         </p>
-        <p>
+</div>
+
+<div class="block my-3">
+
+        <p><mark>
           Several aspects of natural language analysis are handled by this
           algorithm, including expressions, negation words, words written in
           uppercase, ambiguous words, etc.
+          </mark>
         </p>
-        <p>
+</div>
+
+<div class="block">
+        <p><mark>
           In addition, certain tweaks can be applied to the algorithm by
           assigning different 'weights' to different parts of the analysis. For
           example, if someone writes that a place was AWFUL, the 'calculated'
           meaning can be 'negative word' * 2 (e.g. twice as negative as just
           'awful').
+          </mark>
         </p>
+</div>
+<div class="block">
         <p>The following weights were set for the current analysis:</p>
         <ul>
           <li>'negations weight' = 1</li>
           <li>'uppercase weight' = 5</li>
           <li>'confirmation weight' = 3</li>
         </ul>
+</div>
+<div class="block">
         <p>
           Without going into too much detail, the advanced algorithm implements
           the following flow:
         </p>
+</div>
         <ol>
+<div class="block">
           <li>
             Creating a corpus for each review.
             <ul>
@@ -176,14 +244,20 @@
               </li>
             </ul>
           </li>
+</div>
+<div class="block">
           <li>
             Handling expressions and replacing them with the words 'good' or
             'bad', to simplify the upcoming analysis.
           </li>
+</div>
+<div class="block">
           <li>
             Counting positive and negative words and calculating the current
             sentiment
           </li>
+</div>
+<div class="block">
           <li>
             'Negation check': For each positive or negative word found, a
             negation word (such as 'no', 'not'), placed one or two positions
@@ -191,35 +265,49 @@
             word is found, a recalculation of sentiment is performed, changing
             the value from positive to negative and vice versa.
           </li>
+</div>
         </ol>
-        <p>
+
+<div class="block my-3">
+        <p><mark><em>
           A common practice in natural language analysis is the creation of
-          N-grams: 2-grams meaning all possible combinations of two consecutive
+          <strong>n-grams:</strong> 2-grams meaning all possible combinations of two consecutive
           words, 3-grams meaning all possible combinations of three consecutive
           words, etc.
+          </em>
+          </mark>
         </p>
+</div>        
+<div class="block">
         <p>
           I preferred the current method (of search by index), for performance
           reasons.
         </p>
+</div>
         <ol start="5">
+<div class="block">
           <li>
             'Confirmation check': Words such as 'so', 'too', etc., prior to a
             positive or negative word, tend to express a stronger feeling, so if
             such words are found (and according to the pre-set weight), a new
             calculation of the sentiment is performed.
           </li>
+</div>
+<div class="block">
           <li>
             'Uppercase check': Similar to the 'confirmation' logic, if a word is
             written in uppercase, its meaning is emphasized, so according to the
             pre-set weight, another re-calculation is done in such cases.
           </li>
+</div>
         </ol>
+<div class="block my-3">
         <p>
           After running a similar sample of 800000 reviews, the advanced
           algorithm's results are as follows:
         </p>
-        <p>
+</div>
+        <figure>
           <img
             class="aligncenter wp-image-463 size-full"
             title="Advanced Algorithm - Raw Numbers"
@@ -233,8 +321,10 @@
             "
             sizes="(max-width: 487px) 100vw, 487px"
           />
-        </p>
-        <p>
+          <figcaption></figcaption>
+        </figure>
+
+        <figure>
           <img
             class="aligncenter wp-image-462 size-full"
             title="Advanced Algorithm - Percentages"
@@ -248,25 +338,34 @@
             "
             sizes="(max-width: 487px) 100vw, 487px"
           />
-        </p>
+          <figcaption></figcaption>
+        </figure>
+
+<div class="block my-3">
         <p>
           According to the above tables, around <strong>46%</strong> of Negative
           reviews are recognized correctly (1 and 2 stars), and
           <strong>92.3%</strong> of Positive reviews are also recognized
           properly (4 and 5 stars).
         </p>
+</div>
+<div class="block">
         <p>
           The execution of the analysis was performed on a
           <strong>m4.4xlarge</strong> <strong>EC2</strong>
           <strong>instance</strong>, and it took about
           <strong>three hours and 50 minutes</strong>.
         </p>
-        <h3><span style="color: #3366ff">Conclusion</span></h3>
+</div>
+        <h3 class="subtitle has-text-centered"><span style="color: #3366ff">Conclusion</span></h3>
+<div class="block">     
         <p>
           The 'basic' model, as it sometimes happens with 'naïve' models,
           performs relatively well when applied to Yelp's corpus of reviews, at
           least when classifying Positive and Neutral ones.
         </p>
+</div>
+<div class="block">
         <p>
           The 'advanced' model offers certain improvements classifying mainly
           negative reviews, but generally it appears that upset or disappointed
@@ -274,36 +373,51 @@
           sarcasm and cynicism) when writing a negative review, which makes
           classification more complicated.
         </p>
+</div>
+<div class="block">
         <p>
           Further analysis, mainly of negative reviews, is required in order to
           enhance the recognition rates. Also, some performance tuning should be
           done so the advanced algorithm can be executed faster than its current
           execution time.
         </p>
-        <h3><span style="color: #3366ff">Final Note</span></h3>
+</div>
+        <h3 class="subtitle has-text-left"><span style="color: #3366ff">Final Note</span></h3>
+
+<div class="block">
         <p>
           A common methodology implemented in text analysis involves creating a
           Document/Terms matrix, having each document as a row and each column
           being a word in the document (hence the values in the cells
           representing the frequency of each word on each document).
         </p>
+</div>
+<div class="block">
         <p>
           Based on such matrices, different machine learning models are
           implemented, thus trying to learn and predict from the whole set of
           documents the possible outcome (in our case, classifing each review as
           positive, neutral or negative).
         </p>
+</div>
+<div class="block">
         <p>This methodology is sometimes more time and memory-consuming.</p>
+</div>
+<div class="block">
         <p>
           The methodology implemented in this current analysis handles each
           document as an isolated case, being more time and processing
           effective, but missing possible insights from the analysis of a huge
           set of documents as a whole.
         </p>
+</div>
+<div class="block">
         <p>
           Each methodology has pros and cons, and should be implemented
           according to the requirements and limitations of the project.
         </p>
+</div> 
+<div class="block my-4">
         <h3><span style="color: #3366ff">Technical Information</span></h3>
         <ol>
           <li>
@@ -314,6 +428,8 @@
             >
           </li>
         </ol>
+</div>
+<div class="block">
         <ol start="2">
           <li>
             Some rounds of the project were performed on AWS instances (M4
@@ -322,12 +438,15 @@
             process.
           </li>
         </ol>
+</div>
+<div class="block">
         <p>
           Running on an m4.4xlarge EC2 instance, the analysis of 800000 reviews
           took around 3 hours and 50 minutes. Since the execution was with 'spot
           instances', and the cost per hour was less than 20 cents, the total
           cost being less than 80 cents….
         </p>
+</div>
       </div>
       </article>
         </div>
@@ -353,3 +472,9 @@ components: {
 };
 
 </script>
+
+<style scoped>
+mark {
+  background-color: rgb(104, 242, 247);
+}
+</style>
